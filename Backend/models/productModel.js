90abb67d3c36d0196580bfcb4bbc2e57021ab450
Software <soft_int@mm-ilorin.com>
2023-05-05 13:52:53 +0100

    first commit

---
+++ Backend/models/productModel.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true},
+   rating: { type: Number, required: true},
+    comment: { type: String, required: true},
+}, {
+    timeStamps: true,    
+})
+
+const productSchema = mongoose.Schema({
+    
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+        
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,   
+    },
+    description: {
+        type: String,
+        required: true,   
+    },
+
+    reviews: [reviewSchema],
+
+    rating: {
+        type: String,
+        required: true, 
+        default: 0  
+    },
+    numReviews: {
+        type: String,
+        required: true,   
+    },
+    price: {
+        type: String,
+        required: true,
+        default: 0   
+    },
+    countInStock: {
+        type: String,
+        required: true, 
+        default: 0  
+    },
+},
+{
+    timestamps: true,
+}
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
